4862cf21b7d630100611c1ea4238f74700806553
新田 <nics_232144@nicsO365open.onmicrosoft.com>
2026-01-08 15:06:37 +0900

    チャット先ばれ

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -46,6 +46,9 @@ document.addEventListener("DOMContentLoaded", () => {
                         updateGauge();
                     }
                 });
+            
+            await fetch("http://127.0.0.1:5000/ai/speak", { method: "POST" })
+            
         }
     });
 });
