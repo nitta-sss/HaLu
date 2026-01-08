337ea9e69531a279da0477ef465749535e76aea4
新田 <nics_232144@nicsO365open.onmicrosoft.com>
2026-01-08 11:01:06 +0900

    jsからwebにチャット追加

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -1,3 +1,6 @@
+console.log("✅ main.js 読み込まれたよ");
+
+
 document.addEventListener("DOMContentLoaded", () => {
     const voiceBtn = document.getElementById("voiceBtn");
     if (!voiceBtn) return;
@@ -17,7 +20,42 @@ document.addEventListener("DOMContentLoaded", () => {
 
             await fetch("http://127.0.0.1:5000/mic/stop", { method: "POST" });
             await new Promise(r => setTimeout(r, 300));
-            await fetch("http://127.0.0.1:5000/ai/run", { method: "POST" });
+            await fetch("http://127.0.0.1:5000/ai/run", { method: "POST" })
+                .then(res => res.json())
+                .then(data => {
+
+                    console.log("AI結果:", data);
+
+                    // ===== 左の会話欄更新 =====
+                    addMessage("user", data.text);     // Whisper文字起こし
+                    addMessage("bot", data.reply);     // LLM返答
+
+                    // ===== 感情ゲージ更新 =====
+                    window.emotion.x = parseFloat(data.arousal); // 覚醒
+                    window.emotion.y = parseFloat(data.valence); // 快楽
+
+                    if (typeof updateGauge === "function") {
+                        updateGauge();
+                    }
+                });
         }
     });
 });
+
+
+function addMessage(sender, text) {
+    console.log("addMessage 呼ばれた:", sender, text);
+
+    const chatBox = document.querySelector(".chat-box"); //div class chat-boxを参照
+    if (!chatBox) return; //chatBoxがnullの場合終了(安全装置)
+
+    const div = document.createElement("div");//divclass作成
+    div.className = `balloon ${sender}`;  // balloon→吹き出しのcss
+    div.textContent = text;
+    chatBox.appendChild(div);//htmlの
+
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+
+
